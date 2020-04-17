e8bbac691a9a42a048984bf630d90f876976c43f
Ryan <Ryanmcginity123@gmail.com>
2020-04-17 19:04:08 -0400

    Created News and Potd data classes.

---
+++ app/src/main/java/graphical/wireless/espace/ui/NewsData.java
@@ -0,0 +1,47 @@
+package graphical.wireless.espace.ui;
+
+public class NewsData {
+    String headLine;
+    String author;
+    String imageName;
+    String desc;
+
+    public NewsData(String headline, String author, String imageName, String desc) {
+        this.headLine = headline;
+        this.author = author;
+        this.imageName = imageName;
+        this.desc = desc;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getHeadLine() {
+        return headLine;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setHeadLine(String headLine) {
+        this.headLine = headLine;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+}
---
+++ app/src/main/java/graphical/wireless/espace/ui/PotdData.java
@@ -0,0 +1,37 @@
+package graphical.wireless.espace.ui;
+
+public class PotdData {
+    String date;
+    String imageName;
+    String desc;
+
+    public PotdData(String date, String imageName, String desc) {
+        this.date = date;
+        this.imageName = imageName;
+        this.desc = desc;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
